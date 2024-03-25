0d3c288d1f0efd66d7eba6917f7482ac69e6055e
Yerdaulet Seidizhappar <y.seidizhappar@wiponapp.com>
2024-03-25 13:12:48 +0500

    vsa

--- internal/features/chat/router.go
+++ internal/features/chat/router.go
@@ -1,13 +1,19 @@
 package chat
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
 func getRouter() *mux.Router {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/ws/{eventId}", Authorize(joinChatHandler)).Methods("GET")
+	r.HandleFunc("/test", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(writer, "OK")
+		return
+	}).Methods("GET")
 
 	return r
 }
