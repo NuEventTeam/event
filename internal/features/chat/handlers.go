59d527f8c4da1df1a052ba01a3d0bf45d0fce56c
Yerdaulet Seidizhappar <y.seidizhappar@wiponapp.com>
2024-04-19 01:05:45 +0500

    added

--- internal/features/chat/handlers.go
+++ internal/features/chat/handlers.go
@@ -2,25 +2,27 @@ package chat
 
 import (
 	"context"
+	"github.com/gorilla/mux"
 	"log"
 	"math/rand"
 	"net/http"
+	"strconv"
 )
 
 func joinChatHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("here")
 
-	//eventID, err := strconv.ParseInt(mux.Vars(r)["eventId"], 10, 64)
-	//if err != nil {
-	//	w.WriteHeader(http.StatusInternalServerError)
-	//	w.Write([]byte("something went wrogn"))
-	//	log.Println(err)
-	//	return
-	//}
-	ctx := context.WithValue(r.Context(), "eventId", rand.Int63())
+	eventID, err := strconv.ParseInt(mux.Vars(r)["eventId"], 10, 64)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("something went wrogn"))
+		log.Println(err)
+		return
+	}
+	ctx := context.WithValue(r.Context(), "eventId", eventID)
 	ctx = context.WithValue(ctx, "userId", rand.Int63())
 
-	ServeWs(ChatManager, w, r.WithContext(ctx))
+	ServeWs(ChatManager, w, r)
 }
 
 //
