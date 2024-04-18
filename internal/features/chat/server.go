216caf8536498e2fb653cf7c15b49e8c1245c6c1
Yerdaulet Seidizhappar <y.seidizhappar@wiponapp.com>
2024-04-18 23:25:14 +0500

    added

--- internal/features/chat/server.go
+++ internal/features/chat/server.go
@@ -13,8 +13,8 @@ func RunChatServer(port int) error {
 	srv := &http.Server{
 		Handler:      getRouter(),
 		Addr:         fmt.Sprintf(":%d", port),
-		WriteTimeout: 60 * time.Second,
-		ReadTimeout:  60 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ReadTimeout:  10 * time.Second,
 	}
 
 	return srv.ListenAndServe()
