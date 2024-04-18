0b713cd0c8da8bf9d74577a212cfde67b585ad3c
Yerdaulet Seidizhappar <y.seidizhappar@wiponapp.com>
2024-04-19 00:25:48 +0500

    added

--- internal/features/chat/server.go
+++ internal/features/chat/server.go
@@ -2,10 +2,8 @@ package chat
 
 import (
 	"fmt"
-	hd "github.com/gorilla/handlers"
 	"log"
 	"net/http"
-	"os"
 	"time"
 )
 
@@ -20,14 +18,14 @@ func RunChatServer(port int) error {
 		WriteTimeout: 60 * time.Second,
 		ReadTimeout:  60 * time.Second,
 	}
-	headersOk := hd.AllowedHeaders([]string{"X-Requested-With"})
-	originsOk := hd.AllowedOrigins([]string{os.Getenv("ORIGIN_ALLOWED")})
-	methodsOk := hd.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+	//headersOk := hd.AllowedHeaders([]string{"X-Requested-With"})
+	//originsOk := hd.AllowedOrigins([]string{os.Getenv("ORIGIN_ALLOWED")})
+	//methodsOk := hd.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
 
 	// start server listen
 	// with error handling
-	log.Fatal(http.ListenAndServeTLS(fmt.Sprintf(":%d", port), "./certs/domain.crt", "./certs/domain.key", hd.CORS(originsOk, headersOk, methodsOk)(getRouter())))
+	//log.Fatal(http.ListenAndServeTLS(fmt.Sprintf(":%d", port), "./certs/domain.crt", "./certs/domain.key", hd.CORS(originsOk, headersOk, methodsOk)(getRouter())))
 
-	//return srv.ListenAndServe()
-	return srv.ListenAndServeTLS("./certs/domain.crt", "./certs/domain.key")
+	return srv.ListenAndServe()
+	//return srv.ListenAndServeTLS("./certs/domain.crt", "./certs/domain.key")
 }
