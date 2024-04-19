ba02055109a98077e2efc5efefa7425df2cfc8d8
Yerdaulet Seidizhappar <y.seidizhappar@wiponapp.com>
2024-04-19 16:50:19 +0500

    added

--- internal/features/handlers/assets.go
+++ internal/features/handlers/assets.go
@@ -3,7 +3,7 @@ package handlers
 import "github.com/gofiber/fiber/v2"
 
 func (h Handler) SetUpAssetsRoutes(router *fiber.App) {
-	router.Get("/get/:namespace/:key/:filename", h.Assets.GetFile())
+	//router.Get("/get/:namespace/:key/:filename", h.Assets.GetFile())
 	router.Static("/static/", "./static")
 
 }
