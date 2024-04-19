736a804d043e3f0a6c6d8261fb63824ef81439b7
Yerdaulet Seidizhappar <y.seidizhappar@wiponapp.com>
2024-04-19 05:16:30 +0500

    added

--- internal/features/chat/handlers.go
+++ internal/features/chat/handlers.go
@@ -83,7 +83,7 @@ type Messages struct {
 
 func getLastMessages(ctx context.Context, db database.DBTX, userId int64) (map[int64]Messages, error) {
 	query := `select  event_id from event_followers inner join chat_messages on chat_messages.event_id = event_followers.event_id
-group by  event_id where user_id = $1`
+where user_id = $1 group by event_id`
 
 	eventIds := []int64{}
 
